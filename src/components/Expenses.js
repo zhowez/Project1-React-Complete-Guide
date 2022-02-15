92d1e13090f247e448abbd9c415c3cac03605e47
Zach Howes <zhowes14@icloud.com>
2022-02-15 01:34:01 -0500

    added Expense Componant learning how to pass props

---
+++ src/components/Expenses.js
@@ -0,0 +1,34 @@
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
+
+function Expenses(data) {
+  return (
+    <div className="expenses">
+      <ExpenseItem
+        title={data.items[0].title}
+        amount={data.items[0].amount}
+        date={data.items[0].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={data.items[1].title}
+        amount={data.items[1].amount}
+        date={data.items[1].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={data.items[2].title}
+        amount={data.items[2].amount}
+        date={data.items[2].date}
+      ></ExpenseItem>
+
+      <ExpenseItem
+        title={data.items[3].title}
+        amount={data.items[3].amount}
+        date={data.items[3].date}
+      ></ExpenseItem>
+    </div>
+  );
+}
+
+export default Expenses;
