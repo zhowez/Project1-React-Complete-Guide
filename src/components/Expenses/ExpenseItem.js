b63fb2572619b59923c2f8adc6b2230e682e1796
Zach Howes <zhowes14@icloud.com>
2022-02-16 02:31:25 -0500

    removed button which was for learning about ehandl

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,24 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
 
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate.js";
 import Card from "../UI/Card.js";
 
 function ExpenseItem(data) {
-  const [title, setTitle] = useState(data.title);
-
-  const clickHandler = () => {
-    setTitle("Updated!!");
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={data.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2> {title} </h2>
-        <div className="expense-item_∏_price">${data.amount} </div>
+        <h2> {data.title} </h2>
+        <div className="expense-item__price">${data.amount} </div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
