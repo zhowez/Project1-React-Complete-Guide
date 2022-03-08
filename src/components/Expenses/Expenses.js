24237e54502a6098f14bc6ef01fdf061d80214f1
Zach Howes <zhowes14@icloud.com>
2022-03-07 23:27:04 -0500

    Drop down filter now works

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -11,15 +11,20 @@ function Expenses(props) {
     setfilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
-        />{" "}
-        {props.items.map((expense) => (
+        />
+        {filteredExpenses.map((expense) => (
           <ExpenseItem
+            key={expense.id}
             title={expense.title}
             amount={expense.amount}
             date={expense.date}
